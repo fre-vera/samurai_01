a7a0d94d932ef17a756bb62883b2dc72ea11637f
Vera Freiman <fre-vera@yandex.ru>
2025-02-15 22:02:27 +0300

    Added state to a separate file

--- src/index.js
+++ src/index.js
@@ -2,30 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
-
-const dialogs = [
-  {id:1, name: 'Dimych'},
-  {id:2, name: 'Andrey'},
-  {id:3, name: 'Sveta'},
-  {id:4, name: 'Sasha'},
-  {id:5, name: 'Victor'},
-  {id:6, name: 'Valera'},
-];
-
-export const messages = [
-  {id:1, message: 'Hi'},
-  {id:2,  message: 'How is your it-kamasutra'},
-  {id:3,  message: 'Yo'},
-];
-
-const posts = [
-  {id:1, message: 'Hi, how are you', likesCount: 15},
-  {id:2,  message: 'My first post', likesCount: 20},
-];
+import { state } from './components/redux/state';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App messages={messages} dialogs={dialogs} posts={posts} />
+    <App state={state} />
   </React.StrictMode>
 );
