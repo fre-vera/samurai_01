d86212af024f480c9a7b6fbcdc390ea021c6b2a1
Vera Freiman <fre-vera@yandex.ru>
2025-02-16 14:24:54 +0300

    Fixed import useRef

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import useRef from "react";
+import { React, useRef } from "react";
 import classes from './Dialogs.module.scss';
 import { DialogItem } from './DialogItem';
 import { Message } from './Message';
