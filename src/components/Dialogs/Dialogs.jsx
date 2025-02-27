60dc94fcef0ecabce2b858a3c3add12994c91d13
Vera Freiman <fre-vera@yandex.ru>
2025-02-27 19:30:38 +0300

    Refactor Dialogs: remove useRef, integrate Redux, improve markup, add placeholder.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,31 +1,41 @@
-import { React, useRef } from "react";
+import { React } from 'react';
 import classes from './Dialogs.module.scss';
 import { DialogItem } from './DialogItem';
 import { Message } from './Message';
+import { sendMessageCreator, updateNewMessageBodyCreator } from '../redux/dialogs-reducer';
 
 export const Dialogs = (props) => {
   
   const dialogsElements = props.state.dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id} avatar={dialog.avatar} />);
   const messageElements = props.state.messages.map(message => <Message message={message.message} id={message.id} />);
-  const newMessegeElement = useRef();
-  const addMessage = () => {
-    const text = newMessegeElement.current.value;
-    alert(text);
+  const newMessageBody = props.state.newMessageBody;
+  
+  const onSendMessageClick = () => {
+    props.dispatch(sendMessageCreator())
+  };
+
+  const onNewMessageChange = (event) => {
+    let body = event.target.value;
+    props.dispatch(updateNewMessageBodyCreator(body))
   };
 
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogsItem}>
         { dialogsElements }
-        <div>
-          <textarea ref={newMessegeElement}></textarea>
-        </div>
-        <div>
-          <button onClick={addMessage}>Add message</button>
-        </div>
-      </div>
+       </div>
       <div className={classes.message}>
-        {messageElements}
+        <div>{messageElements}</div>
+        <div>
+          <textarea
+            value={newMessageBody}
+            onChange={onNewMessageChange}
+            placeholder="Введите ваше сообщение...">
+          </textarea>
+        </div>
+        <div>
+          <button onClick={onSendMessageClick}>Add message</button>
+        </div>
       </div> 
     </div>
   );
