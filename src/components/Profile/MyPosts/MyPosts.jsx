ca0306912ea88ff75a93448c4ee722950a4dfefd
Vera Freiman <fre-vera@yandex.ru>
2025-02-27 19:31:11 +0300

    Refactoring

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,8 +1,8 @@
-import { React, useRef } from "react";
+import { React, useRef } from 'react';
 import classes from './MyPosts.module.scss';
 import { Post } from './Post';
-import { addPostActionCreator } from "../../redux/state";
-import { updateNewPostTextActionCreator } from "../../redux/state";
+import { addPostActionCreator } from '../../redux/profile-reducer';
+import { updateNewPostTextActionCreator } from '../../redux/profile-reducer';
 
 export const MyPosts = (props) => {
 
