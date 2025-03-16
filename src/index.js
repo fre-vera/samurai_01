6b954190ad34274b206dd84fbc0bb243c1880e65
Vera Freiman <fre-vera@yandex.ru>
2025-03-16 10:14:59 +0300

    Refactor index: use Provider for Redux store, remove manual re-render

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import { store } from './components/redux/store';
+import { store } from './components/redux/redux-store'
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const rerenderEntireTree = (state) => {
   root.render(
   <React.StrictMode>
-    <App state={state} dispatch={store.dispatch.bind(store)}/>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-};
-
-rerenderEntireTree(store.getState());
-
-store.subscribe(() => {
-  const state = store.getState();
-  rerenderEntireTree(state);
-});
