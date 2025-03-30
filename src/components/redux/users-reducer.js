f4a2f7c21b31a50df3970349fddacaf46a599914
Vera Freiman <fre-vera@yandex.ru>
2025-03-30 18:51:06 +0300

    Refactor usersReducer for pagination and dynamic users

--- src/components/redux/users-reducer.js
+++ src/components/redux/users-reducer.js
@@ -1,15 +1,14 @@
 const UNFOLLOW = 'UNFOLLOW';
 const FOLLOW = 'FOLLOW';
 const SET_USERS = 'SET_USERS';
-
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 const initialState = {
-  users: [
-    {id:1, followed: true, fullName: 'Dimych', photoUrl:'https://png.pngtree.com/png-clipart/20240705/original/pngtree-web-programmer-avatar-png-image_15495273.png', status: 'I love my city', location: {country: 'Russia', city: 'St.Peterburg'},},
-    {id:2, followed: false, fullName: 'Andrey', photoUrl:'https://png.pngtree.com/png-clipart/20240705/original/pngtree-web-programmer-avatar-png-image_15495273.png', status: 'I love programming', location: {country: 'Germany', city: 'Berlin'},},
-    {id:3, followed: true, fullName: 'Sveta', photoUrl:'https://pokcer.ru/wp-content/uploads/2014/01/gravatar.webp', status: 'I study react', location: {country: 'Belarus', city: 'Minsk'},},
-    {id:4, followed: false, fullName: 'Sasha', photoUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE6MJGf5aOBXXbTBPdhJjQPWatzY-i7EYObg&s', status: 'The weather is bad outside', location: {country: 'Russia', city: 'Moscow'},},
-  ]
+  users: [],
+  pageSize: 5,
+  totalUsersCount: 0,
+  currentPage: 1,
 };
 
 export const usersReducer = (state = initialState, action) => {
@@ -34,11 +33,18 @@ export const usersReducer = (state = initialState, action) => {
           return user;
         })
       };
-      case SET_USERS: {
-        return { 
-          ...state,users: [...action.users]
-        };
-      }
+    case SET_USERS: 
+      return {
+        ...state,users: action.users
+    }
+    case SET_CURRENT_PAGE:
+      return {
+        ...state, currentPage: action.currentPage
+    }
+    case SET_TOTAL_USERS_COUNT:
+      return {
+        ...state, totalUsersCount: action.setTotalUsersCount
+    }
     default:
       return state;
   }
@@ -47,3 +53,5 @@ export const usersReducer = (state = initialState, action) => {
 export const unFollowAC = (userId) => ({type: UNFOLLOW, userId});
 export const followAC = (userId) => ({type: FOLLOW, userId});
 export const setUsersAC = (users) => ({type: SET_USERS, users});
+export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage});
+export const setTotalUsersCountAC = (setTotalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, setTotalUsersCount});
