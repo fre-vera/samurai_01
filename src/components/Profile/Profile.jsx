2f4ea5b461065f0a41a189b3024d79500d43f6d6
Vera Freiman <fre-vera@yandex.ru>
2025-04-17 18:55:01 +0300

    Fix: correct React import and pass profile props

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,12 @@
-import { React } from 'react';
+import React from 'react';
 import { MyPostsContainer } from './MyPosts/MyPostsContainer';
 import { ProfileInfo } from './ProfileInfo';
 
-export const Profile = () => {
+export const Profile = (props) => {
 
   return (
     <div>
-      <ProfileInfo />
+      <ProfileInfo profile={props.profile} />
       <MyPostsContainer />
     </div>
   );
