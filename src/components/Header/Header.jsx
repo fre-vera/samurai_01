1e3ecebdc3671b71095b93dc1fca78eb2455536b
Vera Freiman <fre-vera@yandex.ru>
2025-04-19 23:10:59 +0300

    Add auth logic and click handler to Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import classes from './Header.module.scss';
+import { NavLink } from 'react-router-dom';
 
-export const Header = () => {
+export const Header = (props) => {
   return (
     <header className={classes.header}>
-      <img src='https://i.pinimg.com/736x/5d/37/59/5d37593086b6b4e6aae2ad8f5b4659b3.jpg' alt='panda' />
+      <img
+        src="https://i.pinimg.com/736x/5d/37/59/5d37593086b6b4e6aae2ad8f5b4659b3.jpg"
+        alt="panda"
+        onClick={props.onLogoClick}
+        style={{ cursor: 'pointer' }}
+      />
+      <div className={classes.loginBlock}>
+        { props.isAuth ? props.login
+          : <NavLink to={'/login'}>Войти</NavLink>}
+      </div>
     </header>
   );
 };
