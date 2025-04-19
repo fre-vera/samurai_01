222588a151f8226eefd517b01f1548b4849ed7a2
Vera Freiman <fre-vera@yandex.ru>
2025-04-19 23:13:11 +0300

    Implement auth state management in Redux

--- src/components/redux/redux-store.js
+++ src/components/redux/redux-store.js
@@ -3,12 +3,14 @@ import { dialogsReducer } from './dialogs-reducer';
 import { profileReducer } from './profile-reducer';
 import { sidebarReducer } from './sidebar-reducer';
 import { usersReducer } from './users-reducer';
+import { authReducer } from './auth-reduser';
 
 export const reducer = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   sidebar: sidebarReducer,
   usersPage: usersReducer,
+  auth: authReducer,
 });
 
 export const store = configureStore({
