5162c2ced3db1f442ddb69baadb8a1d38124590f
Vera Freiman <fre-vera@yandex.ru>
2025-05-21 18:35:04 +0300

    Added TOGGLE_IS_FOLLOWING_PROGRESS action type and followingInProgress array to initial state

--- src/components/redux/users-reducer.js
+++ src/components/redux/users-reducer.js
@@ -4,6 +4,7 @@ const SET_USERS = 'SET_USERS';
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
 const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 const SET_IS_USERS_LOADING = 'SET_IS_USERS_LOADING';
+const TOGGLE_IS_FOLLOWING_PROGRESS = 'TOGGLE_IS_FOLLOWING_PROGRESS';
 
 const initialState = {
   users: [],
@@ -11,6 +12,7 @@ const initialState = {
   totalUsersCount: 0,
   currentPage: 1,
   isUsersLoading: false,
+  followingInProgress: [],
 };
 
 export const usersReducer = (state = initialState, action) => {
@@ -37,18 +39,31 @@ export const usersReducer = (state = initialState, action) => {
       };
     case SET_USERS:
       return {
-        ...state, users: action.users,
+        ...state,
+        users: action.users,
       };
     case SET_CURRENT_PAGE:
       return {
-        ...state, currentPage: action.currentPage,
+        ...state,
+        currentPage: action.currentPage,
       };
     case SET_TOTAL_USERS_COUNT:
       return {
-        ...state, totalUsersCount: action.setTotalUsersCount,
+        ...state,
+        totalUsersCount: action.setTotalUsersCount,
       };
     case SET_IS_USERS_LOADING:
-      return { ...state, isUsersLoading: action.isUsersLoading };
+      return {
+        ...state,
+        isUsersLoading: action.isUsersLoading,
+      };
+    case TOGGLE_IS_FOLLOWING_PROGRESS:
+      return {
+        ...state,
+        followingInProgress: action.isFetching
+          ? [...state.followingInProgress, action.userId]
+          : state.followingInProgress.filter((id) => id !== action.userId),
+      };
     default:
       return state;
   }
@@ -57,6 +72,20 @@ export const usersReducer = (state = initialState, action) => {
 export const unFollowAC = (userId) => ({ type: UNFOLLOW, userId });
 export const followAC = (userId) => ({ type: FOLLOW, userId });
 export const setUsersAC = (users) => ({ type: SET_USERS, users });
-export const setCurrentPageAC = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage });
-export const setTotalUsersCountAC = (setTotalUsersCount) => ({ type: SET_TOTAL_USERS_COUNT, setTotalUsersCount });
-export const setIsUsersLoadingAC = (isUsersLoading) => ({ type: SET_IS_USERS_LOADING, isUsersLoading });
+export const setCurrentPageAC = (currentPage) => ({
+  type: SET_CURRENT_PAGE,
+  currentPage,
+});
+export const setTotalUsersCountAC = (setTotalUsersCount) => ({
+  type: SET_TOTAL_USERS_COUNT,
+  setTotalUsersCount,
+});
+export const setIsUsersLoadingAC = (isUsersLoading) => ({
+  type: SET_IS_USERS_LOADING,
+  isUsersLoading,
+});
+export const toggleFollowingProgress = (isFetching, userId) => ({
+  type: TOGGLE_IS_FOLLOWING_PROGRESS,
+  isFetching,
+  userId,
+});
