7c7e9977291963a8fd9d2af90a133c4f416ebf09
Vera Freiman <fre-vera@yandex.ru>
2025-05-24 17:34:27 +0300

    Added isAuth prop to Profile and pass from ProfileContainer

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { profileApi } from '../../api/api';
 import { Profile } from './Profile';
 import { profileThunk } from '../redux/profile-reducer';
 
@@ -9,10 +8,11 @@ export const ProfileContainer = () => {
   const dispatch = useDispatch();
   const { userId } = useParams();
   const profile = useSelector((state) => state.profilePage.profile);
+  const isAuth = useSelector((store) => store.auth.isAuth);
 
   useEffect(() => {
     dispatch(profileThunk(userId));
   }, [dispatch, userId]);
 
-  return <Profile profile={profile}/>;
+  return <Profile profile={profile} isAuth={isAuth} />;
 };
