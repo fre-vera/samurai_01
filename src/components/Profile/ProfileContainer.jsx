033eb2d17e0c9441b18bf305b0f06c0aee4e95a6
Vera Freiman <fre-vera@yandex.ru>
2025-05-25 12:38:46 +0300

    Adding loading state and Preloader to ProfileContainer

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -4,16 +4,21 @@ import { useParams } from 'react-router-dom';
 import { Profile } from './Profile';
 import { profileThunk } from '../redux/profile-reducer';
 import { withAuthRedirect } from '../hoc/withAuthRedirect';
+import { Preloader } from '../Preloader';
 
 export const ProfileContainer = () => {
   const dispatch = useDispatch();
   const { userId } = useParams();
   const profile = useSelector((state) => state.profilePage.profile);
+  const isLoading = useSelector((state) => state.profilePage.isLoading);
 
   useEffect(() => {
     dispatch(profileThunk(userId));
   }, [dispatch, userId]);
 
+  if (isLoading || !profile) {
+    return <Preloader />;
+  }
   return <Profile profile={profile} />;
 };
 
