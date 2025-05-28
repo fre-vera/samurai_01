e836b647649862d892a3010f75f4682b34d8edd0
Vera Freiman <fre-vera@yandex.ru>
2025-05-28 09:59:52 +0300

    Feat(ProfileContainer): Add status loading and updateStatus handling

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,25 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
+import { Profile } from './Profile';
+import { Preloader } from '../Preloader';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { Profile } from './Profile';
-import { profileThunk } from '../redux/profile-reducer';
+import { getStatus, updateStatus, profileThunk } from '../redux/profile-reducer';
 import { withAuthRedirect } from '../hoc/withAuthRedirect';
-import { Preloader } from '../Preloader';
 
 export const ProfileContainer = () => {
   const dispatch = useDispatch();
   const { userId } = useParams();
   const profile = useSelector((state) => state.profilePage.profile);
   const isLoading = useSelector((state) => state.profilePage.isLoading);
+  const status = useSelector((state) => state.profilePage.status);
+
+  const onUpdateStatus = useCallback(
+    (status) => dispatch(updateStatus(status)),
+    [dispatch],
+  );
 
   useEffect(() => {
     dispatch(profileThunk(userId));
+    dispatch(getStatus(userId));
   }, [dispatch, userId]);
-
   if (isLoading || !profile) {
     return <Preloader />;
   }
-  return <Profile profile={profile} />;
+
+  return <Profile profile={profile} status={status} updateStatus={onUpdateStatus} />;
 };
 
 export default withAuthRedirect(ProfileContainer);
