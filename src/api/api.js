e381fded4f861fb783116c871734a3aac35a7aab
Vera Freiman <fre-vera@yandex.ru>
2025-05-28 09:58:23 +0300

    Fix(ProfileApi): Add getStatus and updateStatus methods

--- src/api/api.js
+++ src/api/api.js
@@ -35,4 +35,12 @@ export const profileApi = {
     return instance.get(`profile/${userId}`)
       .then((response) => response.data);
   },
+  getStatus: (userId) => {
+    return instance.get(`profile/status/${userId}`)
+      .then((response) => response.data);
+  },
+  updateStatus: (status) => {
+    return instance.put(`profile/status`, { status })
+      .then((response) => response.data);
+  },
 };
