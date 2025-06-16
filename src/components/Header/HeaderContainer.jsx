f4d4df2860aa28f51c4e9677eebef93ab93ff888
Vera Freiman <fre-vera@yandex.ru>
2025-06-16 16:19:03 +0300

    Update auth API and HeaderContainer with login/logout support

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -3,21 +3,30 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Header } from './Header';
 import { getAuthUserData } from '../redux/auth-reduser';
+import { logout } from '../redux/auth-reduser';
 
 export const HeaderContainer = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const isAuth = useSelector((state) => state.auth.isAuth);
-  const login = useSelector((state) => state.auth.login);
-
+  const { isAuth, login, userId } = useSelector((state) => state.auth);
 
   useEffect(() => {
     dispatch(getAuthUserData());
   }, [dispatch]);
 
+  useEffect(() => {
+    if (!userId && !isAuth) {
+      navigate('/login');
+    }
+  }, [userId, navigate]);
+
   const handleLogoClick = () => {
     navigate('/profile');
   };
 
-  return <Header isAuth={isAuth} login={login} onLogoClick={handleLogoClick} />;
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
+  return <Header isAuth={isAuth} login={login} logout={handleLogout} onLogoClick={handleLogoClick} />;
 };
