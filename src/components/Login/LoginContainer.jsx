3182c483fbe70ddcff63bd7f80343262519cfeff
Vera Freiman <fre-vera@yandex.ru>
2025-06-16 16:20:41 +0300

    Created LoginContainer with login logic and profile redirect

---
+++ src/components/Login/LoginContainer.jsx
@@ -0,0 +1,29 @@
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { login } from '../redux/auth-reduser';
+import { Login } from './Login';
+
+export const LoginContainer = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const userId = useSelector((state) => state.auth.userId);
+
+  const handleLogin = async (email, password, rememberMe) => {
+    try {
+      await dispatch(login(email, password, rememberMe));
+    } catch (error) {
+      console.error('Login failed:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (userId) {
+      navigate(`/profile/${userId}`);
+    }
+  }, [userId, navigate]);
+
+  return <Login login={handleLogin} />;
+};
