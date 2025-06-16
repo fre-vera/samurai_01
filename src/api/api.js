5b022d93290d4ef70dc2014e1d76255435d3b802
Vera Freiman <fre-vera@yandex.ru>
2025-06-16 16:15:36 +0300

    Refactoring API structure and add auth methods

--- src/api/api.js
+++ src/api/api.js
@@ -23,9 +23,17 @@ export const usersAPI = {
   },
 };
 
-export const headerApi = {
+export const authApi = {
   getAuth: () => {
     return instance.get(`auth/me`)
+      .then((response) => response.data);
+  },
+  login: (email, password, rememberMe = false) => {
+    return instance.post(`auth/login`, { email, password, rememberMe })
+      .then((response) => response.data);
+  },
+  logout: () => {
+    return instance.delete(`auth/login`)
       .then((response) => response.data);
   },
 };
