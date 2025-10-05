8d7159daa769b56b9eb3406d7865d014bd22de91
Vera Freiman <fre-vera@yandex.ru>
2025-10-05 10:57:35 +0300

    Refactor: migrate initializeApp to createAsyncThunk and cleanup reducers

--- src/components/redux/app-reduser.js
+++ src/components/redux/app-reduser.js
@@ -1,31 +1,39 @@
 import { getAuthUserData } from './auth-reduser';
 import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
   initialized: false,
 };
 
+// Thunks
+export const initializeApp = createAsyncThunk(
+  'app/initializeApp',
+  async (_, { dispatch, rejectWithValue }) => {
+    try {
+      await dispatch(getAuthUserData());
+      return true;
+    } catch (error) {
+      return rejectWithValue({
+        message: 'Ошибка при получении данных авторизации',
+        error: error.message,
+      });
+    }
+  },
+);
+
 export const appSlice = createSlice({
   name: 'app',
   initialState,
-  reducers: {
-    initializedSuccess: (state) => {
-      state.initialized = true;
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(initializeApp.fulfilled, (state) => {
+        state.initialized = true;
+      })
+      .addCase(initializeApp.rejected, (state) => {
+        state.initialized = true;
+      });
   },
 },
 );
-
-// // Action creators
-export const { initializedSuccess } = appSlice.actions;
-
-// Thunks
-export const initializeApp = () => async (dispatch) => {
-  try {
-    await dispatch(getAuthUserData());
-  } catch (error) {
-    console.error('Ошибка при получении данных авторизации:', error);
-  } finally {
-    dispatch(initializedSuccess());
-  }
-};
