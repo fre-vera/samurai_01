23b0a79514ed94a5ebd94533127b56f660609f2f
Vera F <fre-vera@yandex.ru>
2025-11-13 19:36:37 +0300

    Feat(FriendsList): Display followed users from usersPage instead of sidebar

--- src/components/Sidebar/FriendsList/FriendsList.jsx
+++ src/components/Sidebar/FriendsList/FriendsList.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
 import { useSelector } from 'react-redux';
 import classes from './FriendsList.module.scss';
 
 export const FriendsList = () => {
-  const friends = useSelector((state) => state.sidebar);
+  const friends = useSelector((state) => state.usersPage.users);
+  const selectFollowedUsers = friends.filter((friend) => friend.followed === true);
 
   return (
     <div className={classes.friendsSection}>
       <h3>Friends</h3>
       <div className={classes.friendsList}>
-        {friends.map((friend) => (
+        {selectFollowedUsers.map((friend) => (
           <div key={friend.id} className={classes.card}>
             <p className={classes.name}>{friend.name}</p>
             <img src={friend.img} alt={friend.name} className={classes.avatar} />
