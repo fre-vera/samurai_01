4023c42ce88bac8e9928065cbf0eabc3273d0fdf
Vera F <fre-vera@yandex.ru>
2025-12-08 19:03:14 +0300

    Feat: Fetch friends on Sidebar mount using getFriendsThunk (was static → now dispatches friends loading on mount)

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import classes from './Sidebar.module.scss';
 import { Navbar } from './Navbar';
 import { FriendsList } from './FriendsList';
+import { getFriendsThunk } from '../redux/friends-reducer';
 
 export const Sidebar = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getFriendsThunk());
+  }, [dispatch]);
+
   return (
     <aside className={classes.sidebar}>
       <Navbar />
