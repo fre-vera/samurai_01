bebc6cb95469f45f6a7e67ab5e2879536ecc2efb
Vera F <fre-vera@yandex.ru>
2025-12-14 10:09:04 +0300

    Feat: Increase users pageSize from 5 to 20 in usersSlice initial state

--- src/components/redux/users-reducer.js
+++ src/components/redux/users-reducer.js
@@ -4,7 +4,7 @@ import { getFriendsThunk } from './friends-reducer';
 
 const initialState = {
   users: [],
-  pageSize: 5,
+  pageSize: 20,
   totalUsersCount: 0,
   currentPage: 1,
   isUsersLoading: false,
