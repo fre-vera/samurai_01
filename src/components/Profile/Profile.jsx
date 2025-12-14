274183d85e0bf4fcbe0cd161088955da3773d0cb
Vera F <fre-vera@yandex.ru>
2025-12-14 10:10:15 +0300

    Feat: Pass follow/unfollow props to ProfileInfo

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,18 @@ import { ProfileInfo } from './ProfileInfo';
 export const Profile = (props) => {
   return (
     <div>
-      <ProfileInfo savePhoto={props.savePhoto} isOwner={props.isOwner} profile={props.profile} status={props.status} updateStatus={props.updateStatus} />
+      <ProfileInfo
+        profile={props.profile}
+        status={props.status}
+        updateStatus={props.updateStatus}
+        isOwner={props.isOwner}
+        savePhoto={props.savePhoto}
+        isFollowed={props.isFollowed}
+        onFollow={props.onFollow}
+        onUnfollow={props.onUnfollow}
+        followingInProgress={props.followingInProgress}
+        userId={props.userId}
+      />
       <MyPostsContainer />
     </div>
   );
